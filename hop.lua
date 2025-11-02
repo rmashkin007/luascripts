fbc9df714ddeed494f32ad35f4b08887078284d8
rmashkin007 <122631923+rmashkin007@users.noreply.github.com>
2025-11-02 15:51:48 +0100

    Update hop.lua

--- hop.lua
+++ hop.lua
@@ -10,6 +10,10 @@ local UsernamesToAvoid = {
     "xXDarkangelOF1",
     "S1apL3gendGG",
     "satoruhoj000",
+	"kazikbeskazikbes",
+	"1_BabelGaming",
+	"TheDyerAnonym",
+	"shiyenoWORKING",
 }
 
 -- Services
